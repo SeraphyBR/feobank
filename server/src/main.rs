001a5284bdce1d3066a02547cb0b0a78ad8d6d02
SeraphyBR <luisjuniorbr@gmail.com>
2021-03-15 21:33:54 -0300

    Updates, create empty tests.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,33 +1,42 @@
-use tokio::net::{TcpListener, TcpStream};
-
 mod account;
 mod session;
 
-use account::Account;
-use session::Session;
+use sqlx::SqlitePool;
+use tokio::net::{TcpListener, TcpStream};
+use dotenv::dotenv;
 
+use session::Session;
 
 #[tokio::main]
 async fn main() {
+    // Load .env
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set!");
+
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    // Create a pool to sqlite database
+    let pool = SqlitePool::connect(&database_url).await.unwrap();
 
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, _addr) = listener.accept().await.unwrap();
+        let db = pool.clone();
 
         tokio::spawn(async move {
-            handle_connection(socket).await;
+            handle_connection(socket, &db).await;
         });
-
     }
 }
 
-async fn handle_connection(stream: TcpStream) {
+async fn handle_connection(stream: TcpStream, db: &SqlitePool) {
+    let mut session = Session::new(stream, db.clone());
     let cpf = String::new();
     let password = String::new();
-    match Session::login(cpf, password) {
-        Ok(mut session) => session.start(stream),
+    match session.login(cpf, password) {
+        Ok(_) => session.start(),
         Err(description) => {
 
         }
--- server/src/session.rs
+++ server/src/session.rs
@@ -1,19 +1,31 @@
+use sqlx::SqlitePool;
 use tokio::net::TcpStream;
 
 use super::account::Account;
 
 pub struct Session {
-    account: Account,
-    conn: TcpStream
+    account: Option<Account>,
+    conn: TcpStream,
+    db: SqlitePool
 }
 
 impl Session {
-    pub fn login(cpf: String, password: String) -> Result<Session, String> {
+
+    pub fn new(conn: TcpStream, db: SqlitePool) -> Session {
+        Session {
+            account: None,
+            conn,
+            db
+        }
+    }
+
+    pub fn login(&mut self, cpf: String, password: String) -> Result<(), String> {
         todo!()
     }
 
-    pub fn start(&mut self, stream: TcpStream) {
-        self.conn = stream;
+    pub fn start(&mut self) {
+        // Responder ao cliente que a sessão foi iniciada, logado com sucesso
+        // Iniciar o loop de ação principal
         todo!()
     }
 }
